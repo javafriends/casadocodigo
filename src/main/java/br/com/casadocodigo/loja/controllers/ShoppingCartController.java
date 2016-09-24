720cac2d8be329029082338ba5e6e611d12f685a
Daniel <daniel@javafriends.com.br>
2016-09-24 16:51:15 -0300

    Aula 3

--- src/main/java/br/com/casadocodigo/loja/controllers/ShoppingCartController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ShoppingCartController.java
@@ -1,13 +1,20 @@
 package br.com.casadocodigo.loja.controllers;
 
+import java.math.BigDecimal;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.casadocodigo.loja.dao.ProdutoDAO;
+import br.com.casadocodigo.loja.models.PaymentData;
 import br.com.casadocodigo.loja.models.Produto;
 import br.com.casadocodigo.loja.models.ShoppingCart;
 import br.com.casadocodigo.loja.models.ShoppingItem;
@@ -16,18 +23,43 @@ import br.com.casadocodigo.loja.models.TipoLivro;
 @Controller
 @RequestMapping("/shopping")
 public class ShoppingCartController {
-	
+
+	private static final Logger logger = LoggerFactory.getLogger(ShoppingCartController.class);
+
 	@Autowired
 	private ProdutoDAO produtoDAO;
-	
+
 	@Autowired
 	private ShoppingCart shoppingCart;
 	
-	@RequestMapping(method=RequestMethod.POST)
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@RequestMapping(method = RequestMethod.POST)
 	public ModelAndView add(Integer produtoId, @RequestParam TipoLivro tipoLivro) {
 		ShoppingItem item = createItem(produtoId, tipoLivro);
 		shoppingCart.add(item);
 		return new ModelAndView("redirect:/produtos");
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String itens() {
+		logger.info("Acessando o carrinho");
+		return "carrinhos/items";
+	}
+
+	@RequestMapping(value="/checkout", method=RequestMethod.POST)
+	public String checkout() {
+		BigDecimal total = shoppingCart.getTotal();
+		String uriToPay = "http://book-payment.herokuapp.com/payment";
+		try {
+			String response = restTemplate.postForObject(uriToPay, new PaymentData(total), String.class);
+			System.out.println(response);
+			return "redirect:/produtos";
+		} catch (HttpClientErrorException exception) {
+			System.out.println("Ocorreu um erro ao criar o Pagamento: " +exception.getMessage());
+			return "redirect:/shopping";
+		}
 	}
 
 	private ShoppingItem createItem(Integer produtoId, TipoLivro tipoLivro) {
